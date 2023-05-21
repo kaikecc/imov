7afd93c94d0b6a7dc8f4b3022d5a24c4380fe77f
kaikecc <kaikecastro@hotmail.com>
2023-05-21 19:42:12 -0300

    fix test

--- imovPlot.py
+++ imovPlot.py
@@ -1,45 +1,55 @@
 from sqlalchemy import create_engine
 import pandas as pd
 import matplotlib.pyplot as plt
-from telegram import Bot, InputFile
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-bot = Bot(token='6274563938:AAE_sZNggKL5le7Zlw3s-U-edOkNzNa-mnA')
-chat_id = '1222662328'
+token = '6274563938:AAE_sZNggKL5le7Zlw3s-U-edOkNzNa-mnA'
+updater = Updater(token=token, use_context=True)
 
-# Cria a conexão com o banco de dados
-engine = create_engine('sqlite:///imoveis.db')
+def send_graph(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
 
-# Carrega os dados da tabela 'julio' no DataFrame
-df_julio = pd.read_sql_table('julio', con=engine)
+    # Cria a conexão com o banco de dados
+    engine = create_engine('sqlite:///imoveis.db')
 
-# Filtra os dados que contêm 'Apartamento' no campo 'Título'
-df_apartamento = df_julio[df_julio['Título'].str.contains('Apartamento')]
+    # Carrega os dados da tabela 'julio' no DataFrame
+    df_julio = pd.read_sql_table('julio', con=engine)
 
-# Agrupa os imóveis por localização e calcula a média e quantidade de apartamentos de cada grupo
-df_grouped = df_apartamento.groupby('Localização')['Valor'].agg(['mean', 'count']).sort_values('count', ascending=False)
+    # Filtra os dados que contêm 'Apartamento' no campo 'Título'
+    df_apartamento = df_julio[df_julio['Título'].str.contains('Apartamento')]
 
-# Cria o gráfico de barras
-fig, ax = plt.subplots(figsize=(10, 6))
-bars = ax.bar(df_grouped.index, df_grouped['mean'], color='blue', alpha=0.5)
+    # Agrupa os imóveis por localização e calcula a média e quantidade de apartamentos de cada grupo
+    df_grouped = df_apartamento.groupby('Localização')['Valor'].agg(['mean', 'count']).sort_values('count', ascending=False)
 
-# Define os rótulos dos eixos x e y
-ax.set_xlabel('Bairros de Brusque-SC')
-ax.set_ylabel('Valor médio (R$)')
-ax.set_title('Relação entre Localização, Valor e Quantidade de Apartamentos')
+    # Cria o gráfico de barras
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = ax.bar(df_grouped.index, df_grouped['mean'], color='blue', alpha=0.5)
 
-# Adiciona as informações de quantidade de apartamentos acima de cada barra
-for i, bar in enumerate(bars):
-    height = bar.get_height()
-    ax.text(bar.get_x() + bar.get_width() / 2., height, str(df_grouped['count'][i]), ha='center', va='bottom')
+    # Define os rótulos dos eixos x e y
+    ax.set_xlabel('Bairros de Brusque-SC')
+    ax.set_ylabel('Valor médio (R$)')
+    ax.set_title('Relação entre Localização, Valor e Quantidade de Apartamentos')
 
-# Mostra o gráfico
-plt.xticks(rotation=90)
-plt.grid()
+    # Adiciona as informações de quantidade de apartamentos acima de cada barra
+    for i, bar in enumerate(bars):
+        height = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width() / 2., height, str(df_grouped['count'][i]), ha='center', va='bottom')
 
-# Salva a figura
-figname = 'grafico.png'
-plt.savefig(figname, dpi=300, bbox_inches='tight')
+    # Mostra o gráfico
+    plt.xticks(rotation=90)
+    plt.grid()
 
-# Envia a imagem pelo Telegram
-with open(figname, 'rb') as photo:
-    bot.send_photo(chat_id=chat_id, photo=InputFile(photo))
+    # Salva a figura
+    figname = 'grafico.png'
+    plt.savefig(figname, dpi=300, bbox_inches='tight')
+    plt.close()
+
+    # Envia a imagem pelo Telegram
+    context.bot.send_photo(chat_id=chat_id, photo=open(figname, 'rb'))
+
+send_command_handler = CommandHandler('send', send_graph)
+updater.dispatcher.add_handler(send_command_handler)
+
+updater.start_polling()
+updater.idle()
